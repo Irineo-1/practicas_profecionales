7d6385e250efc3b01a5d1027b66b7b94d2b1c38f
Irineo-1 <76635517+Irineo-1@users.noreply.github.com>
2023-10-05 15:33:53 -0600

    without rutas php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<?php
-
-    require('controlador/Routing.php');
-
-    Route::add('/', function() {
-        return "hola mundo";
-    }, "get");
-
-    Route::run('/');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
